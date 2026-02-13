c9e3a6e9b9d07259ad4a200b73938f43d5d77131
loresalm <salmina.lorenzo@outlook.com>
2026-02-13 15:20:22 +0100

    firebase config

--- WhereIsMyWind_dashboard/src/firebase.js
+++ WhereIsMyWind_dashboard/src/firebase.js
@@ -4,6 +4,12 @@ import { getFirestore, collection, getDocs } from 'firebase/firestore';
 
 // Your Firebase configuration
 const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
